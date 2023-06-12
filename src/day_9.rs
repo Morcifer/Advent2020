b82c907ec8b1ec5a976aee5d9e0ecb597a8c090c
mor <mor@plotwise.com>
2023-06-12 13:03:19 +0200

    Day 9, basic version.

---
+++ src/day_9.rs
@@ -0,0 +1,92 @@
+use std::str;
+
+use crate::utilities::file_utilities::read_lines;
+
+fn parse_line(line: &str) -> usize {
+    line.parse::<usize>().unwrap()
+}
+
+fn parse_data(file_path: String) -> Vec<usize> {
+    read_lines(file_path)
+        .into_iter()
+        .map(|line| parse_line(line.as_str()))
+        .collect()
+}
+
+pub fn part_1(file_path: String, preamble: usize) -> usize {
+    let numbers = parse_data(file_path);
+
+    for (i, number) in numbers.iter().enumerate().skip(preamble) {
+        let mut match_exists = false;
+
+        for prev_1 in &numbers[i - preamble..i] {
+            for prev_2 in &numbers[i - preamble..i] {
+                if prev_1 + prev_2 == *number {
+                    match_exists = true;
+                    break;
+                }
+            }
+        }
+
+        if !match_exists {
+            return *number;
+        }
+    }
+
+    0
+}
+
+pub fn part_2(file_path: String, preamble: usize) -> usize {
+    let invalid_number = part_1(file_path.clone(), preamble);
+
+    let numbers = parse_data(file_path);
+
+    let contiguous_set = numbers
+        .iter()
+        .enumerate()
+        .filter_map(|(i, number)| {
+            let mut sum = *number;
+            let mut set = vec![*number];
+            let mut index = i;
+
+            while sum < invalid_number && index < numbers.len() - 1 {
+                index += 1;
+                sum += numbers[index];
+                set.push(numbers[index])
+            }
+
+            if sum == invalid_number {
+                Some(set)
+            } else {
+                None
+            }
+        })
+        .next()
+        .unwrap();
+
+    contiguous_set.iter().min().unwrap() + contiguous_set.iter().max().unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    use crate::utilities::file_utilities::get_file_path;
+
+    #[rstest]
+    #[case(true, 127)]
+    #[case(false, 217430975)]
+    fn test_part_1(#[case] is_test: bool, #[case] expected: usize) {
+        let preamble = if is_test { 5 } else { 25 };
+        assert_eq!(expected, part_1(get_file_path(is_test, 9), preamble));
+    }
+
+    #[rstest]
+    #[case(true, 62)]
+    #[case(false, 28509180)]
+    fn test_part_2(#[case] is_test: bool, #[case] expected: usize) {
+        let preamble = if is_test { 5 } else { 25 };
+        assert_eq!(expected, part_2(get_file_path(is_test, 9), preamble));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,25 @@ mod day_5;
 mod day_6;
 #[cfg(test)]
 mod day_7;
+#[cfg(test)]
 mod day_8;
+mod day_9;
 mod utilities;
 
-use crate::day_8::{part_1, part_2};
+use crate::day_9::{part_1, part_2};
 use crate::utilities::file_utilities::get_file_path;
 
 fn main() {
-    let day = 8;
-    let is_test = false;
+    let day = 9;
+    let is_test = true;
+    let preamble = if is_test { 5 } else { 25 };
 
-    println!("Day {day} Part 1: {}", part_1(get_file_path(is_test, day)));
-    println!("Day {day} Part 2: {}", part_2(get_file_path(is_test, day)));
+    println!(
+        "Day {day} Part 1: {}",
+        part_1(get_file_path(is_test, day), preamble)
+    );
+    println!(
+        "Day {day} Part 2: {}",
+        part_2(get_file_path(is_test, day), preamble)
+    );
 }
