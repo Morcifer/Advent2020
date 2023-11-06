8224e3e3b9f62e3b3bf48741780a3e1ada9a1385
mor <mor@plotwise.com>
2023-11-06 13:25:16 +0100

    Day 24: Part 2.

--- src/day_24.rs
+++ src/day_24.rs
@@ -1,7 +1,7 @@
 use crate::utilities::file_utilities::read_lines;
 
 use num::complex::Complex32;
-use rustc_hash::FxHashMap;
+use rustc_hash::{FxHashMap, FxHashSet};
 use std::hash::{Hash, Hasher};
 use std::ops;
 
@@ -77,32 +77,25 @@ fn get_direction_enum(direction: &str) -> Option<Neighbor> {
     }
 }
 
-pub fn part_1(file_path: String) -> i64 {
-    let flip_directions: Vec<String> = read_lines(file_path);
+fn get_tile_configuration(flip_directions: Vec<String>) -> FxHashMap<HexComplex, Color> {
     let mut tiles: FxHashMap<HexComplex, Color> = FxHashMap::default();
 
     tiles.insert(HexComplex::new(0, 0), Color::White);
 
     for flip_direction in flip_directions.clone().into_iter() {
-        println!("Working for {flip_direction}");
-        let mut instruction_index = 0;
+        let mut index = 0;
         let mut current_tile = HexComplex::new(0, 0);
 
-        while instruction_index < flip_direction.len() {
-            println!("At tile {current_tile:?}");
-
-            let new_direction = if let Some(direction) =
-                get_direction_enum(&flip_direction[instruction_index..=instruction_index])
-            {
-                instruction_index += 1;
-                direction
-            } else {
-                let direction =
-                    get_direction_enum(&flip_direction[instruction_index..=instruction_index + 1])
-                        .unwrap();
-                instruction_index += 2;
-                direction
-            };
+        while index < flip_direction.len() {
+            let new_direction =
+                if let Some(direction) = get_direction_enum(&flip_direction[index..=index]) {
+                    index += 1;
+                    direction
+                } else {
+                    let direction = get_direction_enum(&flip_direction[index..=index + 1]).unwrap();
+                    index += 2;
+                    direction
+                };
 
             current_tile = current_tile + get_neighbour_complex(new_direction);
             tiles.entry(current_tile).or_insert(Color::White);
@@ -114,12 +107,86 @@ pub fn part_1(file_path: String) -> i64 {
             Color::Black => tiles.insert(current_tile, Color::White),
         };
     }
+    tiles
+}
 
-    tiles.values().filter(|v| **v == Color::Black).count() as i64
+pub fn part_1(file_path: String) -> i64 {
+    let flip_directions: Vec<String> = read_lines(file_path);
+
+    let tile_configuration = get_tile_configuration(flip_directions);
+
+    tile_configuration
+        .values()
+        .filter(|v| **v == Color::Black)
+        .count() as i64
+}
+
+fn get_neighbours(tile: &HexComplex) -> Vec<HexComplex> {
+    [
+        Neighbor::East,
+        Neighbor::SouthEast,
+        Neighbor::SouthWest,
+        Neighbor::West,
+        Neighbor::NorthWest,
+        Neighbor::NorthEast,
+    ]
+    .into_iter()
+    .map(|n| *tile + get_neighbour_complex(n))
+    .collect::<Vec<_>>()
 }
 
 pub fn part_2(file_path: String) -> i64 {
-    0
+    let flip_directions: Vec<String> = read_lines(file_path);
+    let tile_configuration = get_tile_configuration(flip_directions);
+
+    let mut black_tiles: FxHashSet<HexComplex> = tile_configuration
+        .into_iter()
+        .filter(|(_, v)| *v == Color::Black)
+        .map(|(m, _)| m)
+        .collect();
+
+    for _day in 0..100 {
+        // println!("Day {_day}: {}", black_tiles.len());
+
+        let tile_to_investigate: FxHashSet<HexComplex> = black_tiles
+            .iter()
+            .flat_map(get_neighbours)
+            .chain(black_tiles.iter().cloned())
+            .collect();
+
+        let still_black_tiles: Vec<_> = tile_to_investigate
+            .iter()
+            .filter(|tile| black_tiles.contains(tile))
+            .filter(|tile| {
+                let active_neighbours_count = get_neighbours(tile)
+                    .iter()
+                    .filter(|neighbour| black_tiles.contains(neighbour))
+                    .count();
+                (1..=2).contains(&active_neighbours_count)
+            })
+            .cloned()
+            .collect();
+
+        let new_black_tiles: Vec<_> = tile_to_investigate
+            .iter()
+            .filter(|tile| !black_tiles.contains(tile))
+            .filter(|tile| {
+                let active_neighbours_count = get_neighbours(tile)
+                    .iter()
+                    .filter(|neighbour| black_tiles.contains(neighbour))
+                    .count();
+                active_neighbours_count == 2
+            })
+            .cloned()
+            .collect();
+
+        black_tiles = still_black_tiles
+            .into_iter()
+            .chain(new_black_tiles.into_iter())
+            .collect();
+    }
+
+    black_tiles.len() as i64
 }
 
 #[cfg(test)]
@@ -137,8 +204,8 @@ mod tests {
     }
 
     #[rstest]
-    #[case(true, 0)]
-    #[case(false, 0)]
+    #[case(true, 2208)]
+    #[case(false, 3445)]
     fn test_part_2(#[case] is_test: bool, #[case] expected: i64) {
         assert_eq!(expected, part_2(get_file_path(is_test, 24, None)));
     }
